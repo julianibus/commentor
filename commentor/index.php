6056c4cf1ff474866cce07c557ffde43fda2703e
Julian Wienand <jwienand@aol.com>
2016-10-17 23:46:05 +0200

    add additional files

--- commentor/index.php
+++ commentor/index.php
@@ -15,15 +15,13 @@ $caller = $_GET["ref"];
 <?php echo '<form action="/commentor/post.php?ref=' . $caller . '" method="post">';  ?>
 <form action="/commentor/post.php" method="post">
 
-    <p>Your name: <input type="text" name="name" /></p>
+    <p><input type="text" name="name" style="width: 24%;" placeholder="name"/><input style="width: 24%;" type="text" name="email" placeholder="email"/></p>
 
-    <p>Your email: <input type="text" name="email" /></p>
+    <p class="antispam">Leave this empty: <input type="text" name="url" style="width: 24%;"/></p>
 
-    <p class="antispam">Leave this empty: <input type="text" name="url" /></p>
-
-    <p><textarea name="message"></textarea></p>
-
-    <p><input type="submit" value="Send" /></p>
+    <p><textarea name="message" style="min-width:100px; width: 48%;" placeholder="comment"></textarea></p>
+    <img src="res/submit.png" onclick="document.forms['formName'].submit();">
+    </p>
 
 </form>
 
