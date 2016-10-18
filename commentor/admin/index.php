b9f622e6d17701222948fb0065d7bc50a5038e85
Julian Wienand <jwienand@aol.com>
2016-10-18 15:56:16 +0200

    add password protection for admin interface

--- commentor/admin/index.php
+++ commentor/admin/index.php
@@ -1,3 +1,12 @@
+<?php
+$username = "admin";
+$password = "commentor";
+$nonsense = "sostarbenwirumungetrenntewigeinigohneend";
+
+if (isset($_COOKIE['PrivatePageLogin'])) {
+   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+?>
+
 <html>
 <head>
 <link rel="stylesheet" href="../template.css">
@@ -11,6 +20,7 @@ $conf = parse_ini_file('../config',1);
 $datafolder = "../" . $conf["datafolder"];
 
 $files = scandir($datafolder);
+$filecounter = 0;
 foreach($files as $file) {
 	if ($file == "." || $file == "..") {
 		continue;
@@ -20,6 +30,7 @@ foreach($files as $file) {
 	{
 		continue;
 	}
+	$filecounter += 1;
 	$fileo = file($path);
 	$realurl = urldecode(pathinfo($file, PATHINFO_FILENAME));
 	echo "<br><br><b>Page: <a href='" . $realurl . "'>" . $realurl . "</a></b>";
@@ -42,8 +53,43 @@ foreach($files as $file) {
 	echo "<br><br>(" . (string) $commentscounter . " comments)";
 }
 
+if ($filecounter == 0) {
+	echo "No comments have been posted yet. Spread the word and check back later.";
+}
+
 
 ?>
 
 </body>
 </html>
+
+
+<?php
+      exit;
+   } else {
+      echo "Bad Cookie.";
+      exit;
+   }
+}
+
+if (isset($_GET['p']) && $_GET['p'] == "login") {
+   if ($_POST['user'] != $username) {
+      echo "Sorry, that username does not match.";
+      exit;
+   } else if ($_POST['keypass'] != $password) {
+      echo "Sorry, that password does not match.";
+      exit;
+   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
+      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
+      header("Location: $_SERVER[PHP_SELF]");
+   } else {
+      echo "Sorry, you could not be logged in at this time.";
+   }
+}
+?>
+<h1>Commentor</h1>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
+<label><input type="text" name="user" id="user" /> Name</label><br />
+<label><input type="password" name="keypass" id="keypass" /> Password</label><br />
+<input type="submit" id="submit" value="Login" />
+</form>
